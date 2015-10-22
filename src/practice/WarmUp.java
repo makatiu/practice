b43445ec2e28348c4cc6f64888e79596ab86a1c1
Mark Tiu <makatiu@yahoo.com>
2015-10-22 08:14:10 +0800

    initial commit

---
+++ src/practice/WarmUp.java
@@ -0,0 +1,16 @@
+package practice;
+
+public class WarmUp {
+
+	public static void main(String[] args) {
+
+		String mark = "hi hello";
+		
+		
+		
+		System.out.println(mark.substring(0,2).equals("hi"));
+		
+
+	}
+
+}
