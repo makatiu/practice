f5c908dbc7c4edfe542c4774cf25b8826ab11a9f
markangelotiu <markctiu@gmail.com>
2016-04-03 14:55:09 +0800

    change string

--- src/practice/WarmUp.java
+++ src/practice/WarmUp.java
@@ -4,10 +4,8 @@ public class WarmUp {
 
 	public static void main(String[] args) {
 
-		String mark = "hi hello";
-		
-		
-		
+		String mark = "hi hello1";
+
 		System.out.println(mark.substring(0,2).equals("hi"));
 		
 
